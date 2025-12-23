c8af22ca9e11e594267aa43c9f6e9b2a959d0e17
CarlosMamaniAchinquipa <85691606+carlos456dddd@users.noreply.github.com>
2025-12-23 17:32:46 -0500

    Client and Server

---
+++ conection-tcp.c
@@ -0,0 +1,24 @@
+
+     
+#include <winsock2.h>
+#include <windows.h>    
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    WSADATA wsaData;
+
+    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
+        fprintf(stderr, "WSAStartup failed.\n");
+        exit(1);
+    }
+
+    if (LOBYTE(wsaData.wVersion) != 2 ||
+        HIBYTE(wsaData.wVersion) != 2)
+    {
+        fprintf(stderr,"Version 2.2 of Winsock not available.\n");
+        WSACleanup();
+        exit(2);
+    }
+   return 0;
+}
